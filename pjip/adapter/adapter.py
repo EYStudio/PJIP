a8a8a9bc4d8623c0c69875029dda06072f64d033
Errorsia <Errorsia@outlook.com>
2026-02-01 21:36:52 +0800

    change: move adapters

--- pjip/adapter/adapter.py
+++ pjip/adapter/adapter.py
@@ -3,10 +3,11 @@
 from typing import Iterable
 
 from PySide6.QtCore import QObject, Signal, QThreadPool
-from PySide6.QtGui import QGuiApplication
 
 from pjip.config import build_config
 from pjip.core.enums import PidStatus
+from .action import SuspendStudentmainAdapter, StartStudentmainAdapter, CleanIFEODebuggersAdapter, \
+    CopyToClipboardAdapter
 from .dispatcher import TaskDispatcher
 
 from .polling import MonitorAdapter, SuspendMonitorAdapter, GetStudentmainPasswordAdapter, UpdateAdapter, \
@@ -308,54 +309,3 @@ class TerminateProcessAdapter(QObject):
 
         pids = self.logic.get_pid_from_process_name(process_name)
         self.pid_adapter.run_sync(pids)
-
-
-class StartStudentmainAdapter:
-    def __init__(self, logic):
-        super().__init__()
-        self.logic = logic
-
-    def start(self):
-        return self.logic.start_studentmain()
-
-
-class SuspendStudentmainAdapter:
-    def __init__(self, logic):
-        super().__init__()
-        self.logic = logic
-
-    def start(self):
-        pids = self.logic.get_pid_from_process_name(build_config.E_CLASSROOM_PROGRAM_NAME)
-
-        if pids is None:
-            print(f'{build_config.E_CLASSROOM_PROGRAM_NAME} not found')
-
-        for pid in pids:
-            suspend_state = self.logic.is_suspended(pid)
-            if suspend_state:
-                self.resume(pid)
-            else:
-                self.suspend(pid)
-
-    def suspend(self, pid):
-        self.logic.suspend_process(pid)
-
-    def resume(self, pid):
-        self.logic.resume_process(pid)
-
-
-class CleanIFEODebuggersAdapter:
-    def __init__(self, logic):
-        super().__init__()
-        self.logic = logic
-
-    def start(self):
-        self.logic.clean_ifeo_debuggers()
-
-
-class CopyToClipboardAdapter:
-    def __init__(self):
-        self.clipboard = QGuiApplication.clipboard()
-
-    def copy_to_clipboard(self, content: str):
-        self.clipboard.setText(content)
