9760b192e3e67425dc2491cc108866395eef79b0
Errorsia <Errorsia@outlook.com>
2026-02-05 16:02:13 +0800

    feat: svg_utils.py

---
+++ pjip/gui/utils/svg_utils.py
@@ -0,0 +1,14 @@
+from PySide6.QtGui import QIcon, QPixmap, QPainter
+from PySide6.QtSvg import QSvgRenderer
+from PySide6.QtCore import QByteArray, Qt
+
+def svg_to_icon(svg_text: str, size: int = 256) -> QIcon:
+    renderer = QSvgRenderer(QByteArray(svg_text.encode("utf-8")))
+    pixmap = QPixmap(size, size)
+    pixmap.fill(Qt.GlobalColor.transparent)
+
+    painter = QPainter(pixmap)
+    renderer.render(painter)
+    painter.end()
+
+    return QIcon(pixmap)
