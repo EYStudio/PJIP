10a7d0fb1c574e00cfab61fcf1db137807d76ee6
Errorsia <Errorsia@outlook.com>
2026-02-06 21:27:40 +0800

    feat: studio info

--- pjip/gui/pages/about_page.py
+++ pjip/gui/pages/about_page.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy, QHBoxLayout
 
 from ..utils.q_pixmap_utils import make_round_pixmap
 from ..utils.svg_utils import svg_to_pixmap
@@ -13,44 +13,66 @@ class AboutPage(QWidget):
         super().__init__()
         self.page_name = 'About'
         self.studio_icon = None
+        self.studio_name = None
 
         self.init_ui()
 
     def init_ui(self):
         main_layout = QVBoxLayout()
-        main_layout.setContentsMargins(3, 3, 3, 3)
+        main_layout.setContentsMargins(10, 3, 10, 3)
         main_layout.setSpacing(5)
+
+        wrapper = QWidget()
+        wrapper_layout = QHBoxLayout(wrapper)
+        wrapper.setObjectName('wrapper')
 
         self.studio_icon = QLabel()
 
         img_pixmap = svg_to_pixmap(SVG_COLORED_LOGO)
-
         rounded = make_round_pixmap(img_pixmap, 256)
 
         self.studio_icon.setPixmap(rounded)
         self.studio_icon.setFixedSize(290, 270)
         self.studio_icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.studio_icon.setStyleSheet("""
-            border: 5px solid #dddddd;
-            padding-left: 10px;
-            border-radius: 20px""")
+        wrapper_layout.addWidget(self.studio_icon)
+        wrapper_layout.setContentsMargins(30, 0, 0, 0)
+        wrapper.setStyleSheet("""
+            #wrapper{
+                border: 5px solid #dddddd;
+                border-radius: 20px;
+            }
+            """)
+
+        studio_info_widget = QWidget()
+        studio_info_widget.setObjectName('studio_info_widget')
+        studio_info_layout = QVBoxLayout(studio_info_widget)
 
         self.studio_name = QLabel()
         self.studio_name.setText('Eystudio')
+        self.studio_name.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.studio_name.setStyleSheet("""
-            border-radius: 10px;
-            font-size: 24px;
-            border: 3px solid #cccccc;
+            font-size: 36px;
             color: #555555;   
-            padding: 10px;
         """)
         self.studio_name.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
-        studio_name_font = QFont("Segoe UI")
-        self.studio_name.setFont(studio_name_font)
+        studio_info_font = QFont("Segoe UI")
+        self.studio_name.setFont(studio_info_font)
 
-        main_layout.addWidget(self.studio_icon, alignment=Qt.AlignmentFlag.AlignHCenter)
-        main_layout.addWidget(self.studio_name, alignment=Qt.AlignmentFlag.AlignHCenter)
+        studio_info_layout.addWidget(self.studio_name, alignment=Qt.AlignmentFlag.AlignHCenter)
+        studio_info_widget.setStyleSheet("""
+            #studio_info_widget{
+                border-radius: 10px;
+                font-size: 36px;
+                border: 3px solid #cccccc;
+                padding: 10px;
+            }
+        """)
+
+        # slogan = None ; motto?
+
+        main_layout.addWidget(wrapper, alignment=Qt.AlignmentFlag.AlignHCenter)
+        main_layout.addWidget(studio_info_widget)
 
         self.setLayout(main_layout)
