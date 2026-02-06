1caa6ae66a12be6c20f0e7491226580913dbd4fb
Errorsia <Errorsia@outlook.com>
2026-02-06 19:06:26 +0800

    feat: make_round_rect_pixmap

---
+++ pjip/gui/utils/q_pixmap_utils.py
@@ -0,0 +1,25 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap, QPainter, QPainterPath
+
+
+def make_round_rect_pixmap(pixmap: QPixmap, size: int, radius: int = 0) -> QPixmap:
+    pixmap = pixmap.scaled(
+        size, size,
+        Qt.AspectRatioMode.KeepAspectRatioByExpanding,
+        Qt.TransformationMode.SmoothTransformation
+    )
+
+    rounded = QPixmap(size, size)
+    rounded.fill(Qt.GlobalColor.transparent)
+
+    painter = QPainter(rounded)
+    painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+    path = QPainterPath()
+    path.addRoundedRect(0, 0, size, size, radius, radius)
+
+    painter.setClipPath(path)
+    painter.drawPixmap(0, 0, pixmap)
+    painter.end()
+
+    return rounded
