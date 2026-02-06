dff84b757529c3308ab4533dbc45ae489def98ec
Errorsia <Errorsia@outlook.com>
2026-02-06 18:46:22 +0800

    feat: svg_to_pixmap

--- pjip/gui/utils/svg_utils.py
+++ pjip/gui/utils/svg_utils.py
@@ -2,13 +2,23 @@ from PySide6.QtGui import QIcon, QPixmap, QPainter
 from PySide6.QtSvg import QSvgRenderer
 from PySide6.QtCore import QByteArray, Qt
 
-def svg_to_icon(svg_text: str, size: int = 256) -> QIcon:
+
+def svg_to_pixmap(svg_text: str, size: int = 256) -> QPixmap:
+    """Render SVG text into a QPixmap with transparent background."""
     renderer = QSvgRenderer(QByteArray(svg_text.encode("utf-8")))
+
     pixmap = QPixmap(size, size)
     pixmap.fill(Qt.GlobalColor.transparent)
 
     painter = QPainter(pixmap)
     renderer.render(painter)
+    # renderer.render(painter, pixmap.rect())
     painter.end()
 
+    return pixmap
+
+
+def svg_to_icon(svg_text: str, size: int = 256) -> QIcon:
+    """Convert SVG text directly into a QIcon."""
+    pixmap = svg_to_pixmap(svg_text, size)
     return QIcon(pixmap)
