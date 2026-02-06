b8b24bd5e65d47cfd580220b4b12f57a99d14c07
Errorsia <Errorsia@outlook.com>
2026-02-06 19:38:23 +0800

    update: add studio icon

--- pjip/gui/pages/about_page.py
+++ pjip/gui/pages/about_page.py
@@ -1,8 +1,10 @@
-from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QWidget, QVBoxLayout
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 
 from .page_updating import PageUpdating
-
+from ..utils.q_pixmap_utils import make_round_pixmap
+from ..utils.svg_utils import svg_to_pixmap
+from ..resources import SVG_COLORED_LOGO
 
 class AboutPage(QWidget):
     ui_change = Signal(str, object)
@@ -15,11 +17,21 @@ class AboutPage(QWidget):
 
     def init_ui(self):
         main_layout = QVBoxLayout()
-        # main_layout.setContentsMargins(3, 3, 3, 3)
-        # main_layout.setSpacing(5)
+        main_layout.setContentsMargins(3, 3, 3, 3)
+        main_layout.setSpacing(5)
 
-        page_updating = PageUpdating()
+        self.studio_icon = QLabel()
 
-        main_layout.addWidget(page_updating)
+        img_pixmap = svg_to_pixmap(SVG_COLORED_LOGO)
+
+        rounded = make_round_pixmap(img_pixmap, 256)
+
+        self.studio_icon.setPixmap(rounded)
+        self.studio_icon.setFixedSize(270, 270)
+        self.studio_icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.studio_icon.setStyleSheet('border: 5px solid #dddddd;')
+
+        main_layout.addWidget(self.studio_icon)
 
         self.setLayout(main_layout)
