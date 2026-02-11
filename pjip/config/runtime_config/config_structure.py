7439ebfdeaeb2da4f35142d1cad67f60f487c635
Errorsia <Errorsia@outlook.com>
2026-02-11 10:28:29 +0800

    feat: config content

--- pjip/config/runtime_config/config_structure.py
+++ pjip/config/runtime_config/config_structure.py
@@ -28,4 +28,9 @@ class Debug:
 @dataclass
 class ConfigRoot:
     app: App = field(default_factory=App)
+    logging: Logging = field(default_factory=Logging)
+    process: Process = field(default_factory=Process)
+    ui: UI = field(default_factory=UI)
+    features: Features = field(default_factory=Features)
+    debug: Debug = field(default_factory=Debug)
 
