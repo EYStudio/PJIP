05355f539d3b1f6b31f2a56e68fad5be01c2ac76
Errorsia <Errorsia@outlook.com>
2026-02-15 10:33:02 +0800

    feat: base dir, config path

--- pjip/app/constants.py
+++ pjip/app/constants.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+
+def get_base_dir():
+    if getattr(sys, 'frozen', False):
+        # Running in PyInstaller bundle
+        return os.path.dirname(sys.executable)
+    else:
+        # Running in normal Python environment
+        return os.path.dirname(os.path.abspath(__file__))
 
 
 E_CLASSROOM_NAME = 'studentmain'
@@ -5,3 +16,8 @@ E_CLASSROOM_PROGRAM_NAME = E_CLASSROOM_NAME + '.exe'
 
 STUDENTMAIN_NAME = 'studentmain'
 IS_E_CLASSROOM_STUDENTMAIN = STUDENTMAIN_NAME.lower() == E_CLASSROOM_NAME.lower()
+
+BASE_DIR = get_base_dir()
+CONFIG_PATH = os.path.join(BASE_DIR, "PJIPConfig.toml")
+
+print(f"Config Path: {CONFIG_PATH}")
