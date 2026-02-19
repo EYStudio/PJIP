dc9b9d70e3ffb45924114298dac8ed47dd927b67
Errorsia <Errorsia@outlook.com>
2026-02-19 09:01:55 +0800

    feat: RuntimeConfigManager

--- pjip/config/runtime_config/runtime_config_manager.py
+++ pjip/config/runtime_config/runtime_config_manager.py
@@ -1,7 +1,21 @@
 from .config_structure import ConfigRoot
 from .serialize_config import serialize_config_to_dict
 
-
+# Todo: RuntimeConfigManager init
 class RuntimeConfigManager:
     def __init__(self):
+        self.config_root = ConfigRoot()
+        self.serialize_config = serialize_config_to_dict
+
+        self.config_output = self.serialize_config(self.config_root)
+        print(self.config_root)
+        print(self.config_output)
+
+    def initialize_config(self):
+        pass
+
+    def read_config(self):
+        pass
+
+    def anti_serialize_config(self):
         pass
