25cd2c8c0f082a34c474d7824cf9fb7ee62e2027
Errorsia <Errorsia@outlook.com>
2026-02-21 20:05:53 +0800

    feat: handle Enum

--- pjip/config/runtime_config/config_input_utils.py
+++ pjip/config/runtime_config/config_input_utils.py
@@ -4,8 +4,20 @@ def deserialize_dataclass(cls, data: dict):
     for f in fields(cls):
         name = f.name
         type_ = f.type
+
         if name not in data:
             kwargs[name] = f.default
             continue
 
         raw_value = data[name]
+
+        # handle Enum
+        if isinstance(type_, type) and issubclass(type_, Enum):
+            try:
+                kwargs[name] = type_(raw_value)
+            except Exception:
+                if hasattr(type_, "DEFAULT"):
+                    kwargs[name] = type_.DEFAULT
+                else:
+                    raise ValueError(f"{cls.__name__}.{name} invalid enum value: {raw_value}")
+            continue
