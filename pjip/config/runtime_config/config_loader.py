81e8bfb2c81b6586c089f8c95d56bfd70b28e920
Errorsia <Errorsia@outlook.com>
2026-02-22 10:06:52 +0800

    feat: read config

--- pjip/config/runtime_config/config_loader.py
+++ pjip/config/runtime_config/config_loader.py
@@ -2,3 +2,12 @@ class ConfigIO:
     def __init__(self, config_path, config_class):
         self.config_path = config_path
         self.config_class = config_class
+
+    def read_config(self):
+        """read TOML config"""
+        try:
+            with open(self.config_path, "rb") as f:
+                return tomllib.load(f)
+        except Exception as e:
+            print(f"Failed to read config: {e}")
+            return None
