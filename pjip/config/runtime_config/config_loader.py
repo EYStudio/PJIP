9beb8603343b4255f0a3f39fc0ed1e12659c0d5e
Errorsia <Errorsia@outlook.com>
2026-02-22 10:13:22 +0800

    feat: serialize_comments

--- pjip/config/runtime_config/config_loader.py
+++ pjip/config/runtime_config/config_loader.py
@@ -26,3 +26,19 @@ class ConfigIO:
         except Exception as e:
             print(f"Failed to read config: {e}")
             return None
+    def serialize_comments(self):
+        def serialize_line(comment_line):
+            if comment_line.startswith('# '):
+                comment_line = comment_line[2:]
+            return '# ' + comment_line + '\n'
+
+        toml_comments = ''
+        if self.comments:
+            for comment in self.comments:
+                # lines = comment.splitlines()
+                toml_comments += serialize_line(comment)
+
+
+            toml_comments += '\n'
+
+        return toml_comments
