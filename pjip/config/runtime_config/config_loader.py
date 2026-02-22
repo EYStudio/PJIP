76e405a55b09019941ce0c130387912bd3f1a4ce
Errorsia <Errorsia@outlook.com>
2026-02-22 10:07:12 +0800

    feat: load

--- pjip/config/runtime_config/config_loader.py
+++ pjip/config/runtime_config/config_loader.py
@@ -1,7 +1,22 @@
+import os
+import tomllib
+
+from .config_input_utils import deserialize_dataclass
+
+
 class ConfigIO:
     def __init__(self, config_path, config_class):
         self.config_path = config_path
         self.config_class = config_class
+
+    def load(self):
+        if not os.path.exists(self.config_path):
+            return self.config_class()
+        config_dict = self.read_config()
+        if config_dict is not None:
+            return deserialize_dataclass(self.config_class, config_dict)
+        else:
+            return self.config_class()
 
     def read_config(self):
         """read TOML config"""
