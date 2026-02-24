be19788ac3f91ddcadf4535072f70a1e3bc59015
Errorsia <Errorsia@outlook.com>
2026-02-24 18:23:59 +0800

    change: move TerminatePIDAdapter and TerminateProcessAdapter

--- pjip/adapter/adapter.py
+++ pjip/adapter/adapter.py
@@ -1,21 +1,18 @@
 # adapter.py
 # from threading import Thread
-from typing import Iterable
 
 from PySide6.QtCore import QObject, Signal, QThreadPool
 
 from pjip.core.enums import PidStatus
-
 from .action import SuspendStudentmainAdapter, StartStudentmainAdapter, CleanIFEODebuggersAdapter, \
     CopyToClipboardAdapter, EditKillMethodAdapter
 from .dispatcher import TaskDispatcher
 from .polling import MonitorAdapter, SuspendMonitorAdapter, GetStudentmainPasswordAdapter, UpdateAdapter, \
     RunTaskmgrAdapter
 from .polling_manager import PollingManager
-from .runner import TerminatePIDTask
+from .task import TerminatePIDAdapter, TerminateProcessAdapter
 from ..app.constants import E_CLASSROOM_PROGRAM_NAME
 from ..config import RuntimeConfigManager
-from ..config.runtime_config.config_structure import ConfigRoot
 
 
 class AdapterManager(QObject):
@@ -60,7 +57,8 @@ class AdapterManager(QObject):
         self.update_adapter = UpdateAdapter(self.logic)
         self.polling.add(self.update_adapter)
 
-        self.terminate_pid_adapter = TerminatePIDAdapter(self.logic, self.runtime_status.pid, self.dispatcher, self.config_object)
+        self.terminate_pid_adapter = TerminatePIDAdapter(self.logic, self.runtime_status.pid, self.dispatcher,
+                                                         self.config_object)
 
         self.terminate_process_adapter = TerminateProcessAdapter(self.logic, self.runtime_status.current_process_name,
                                                                  self.terminate_pid_adapter)
@@ -232,94 +230,3 @@ class TerminateCustomProcessAdapter(QObject):
         if not process_name.lower().endswith(".exe"):
             return process_name + ".exe"
         return process_name
-
-
-# ################
-
-
-class TerminatePIDAdapter(QObject):
-    change = Signal(str)
-
-    def __init__(self, logic, current_pid, dispatcher, config_object: ConfigRoot):
-        super().__init__()
-        self.logic = logic
-        self.current_pid = current_pid
-        self.dispatcher = dispatcher
-        self.config_object = config_object
-
-    # Bugs here: self.logic.terminate_process cannot accept tuple, on int
-    # def run_async(self, pids):
-    #     other_pids = self.split_current_pid(pids)
-    #     if not other_pids:
-    #         return
-    #
-    #     task = FutureRunnable(
-    #         self.logic.terminate_process,
-    #         other_pids,
-    #         callback=lambda _: self.change.emit("done"),
-    #         error_callback=lambda e: self.change.emit(str(e))
-    #     )
-    #
-    #     self.dispatcher.submit(task, priority=10)
-
-    def run_async(self, pids):
-        valid_pids = self.format_pids(pids)
-        other_pids = self.split_current_pid(valid_pids)
-        kill_method = self.config_object.process.kill_method
-        if other_pids:
-            task = TerminatePIDTask(self.logic, other_pids, kill_method)
-            self.dispatcher.submit(task)
-
-
-    def run_sync(self, pids):
-        valid_pids = self.format_pids(pids)
-        other_pids = self.split_current_pid(valid_pids)
-        kill_method = self.config_object.process.kill_method
-        if other_pids:
-            task = TerminatePIDTask(self.logic, other_pids, kill_method)
-            task.run()
-
-    @staticmethod
-    def format_pids(pids: int | Iterable[int]):
-        if isinstance(pids, int):
-            return (pids,)
-        return tuple(pids)
-
-    def split_current_pid(self, pids):
-        """Check if pids contains current_pid and return the rest."""
-        if self.current_pid in pids:
-            self.change.emit('Cannot terminate the current process(form pid)')
-            print('Cannot terminate the current process(form pid)')
-        other_pids = [pid for pid in pids if pid != self.current_pid]
-        return other_pids
-
-
-class TerminateProcessAdapter(QObject):
-    """Terminate process, rely on PID adapter"""
-    change = Signal(str)
-
-    def __init__(self, logic, current_process_name, pid_adapter: TerminatePIDAdapter, /):
-        super().__init__()
-        self.logic = logic
-        self.pid_adapter = pid_adapter
-        self.current_process_name = current_process_name
-
-    def run_async(self, process_name):
-        # if process_name == self.current_process_name:
-        #     self.change.emit('Cannot terminate the current process')
-        #     print('Cannot terminate the current process')
-        #     return
-
-        pids = self.logic.get_pid_from_process_name(process_name)
-        if pids:
-            self.pid_adapter.run_async(pids)
-        else:
-            print(f'Invalid pids: {pids}')
-
-    def run_sync(self, process_name):
-        # if process_name == self.current_process_name:
-        #     self.change.emit('Cannot terminate the current process')
-        #     return
-
-        pids = self.logic.get_pid_from_process_name(process_name)
-        self.pid_adapter.run_sync(pids)
